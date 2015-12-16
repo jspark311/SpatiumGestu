16ab93e0d0c563e8584ceed0df6793e6f1db1267
jspark311 <josh.lindsay@gmail.com>
2015-12-16 08:26:32 -0700

    Found and fixed a few doc/makefile mistakes while I was working on ViamSonus.

--- src/FirmwareDefs.h
+++ src/FirmwareDefs.h
@@ -6,11 +6,6 @@ Date:   2015.06.01
 
 This is one of the files that the application author is required to provide. 
 This is where definition of (application or device)-specific parameters ought to go.
-
-This is an example file for building firmware on linux. Anticipated target is a Raspi.
-  but that shouldn't matter too much. These settings are only typically relevant for
-  reasons of memory-constraint, threading model (if any), or specific features that
-  this hardware can support.
 */
 
 #ifndef __FIRMWARE_DEFS_H
