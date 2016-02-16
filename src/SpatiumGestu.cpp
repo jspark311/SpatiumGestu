119ddac72358b0769ce37bd757d8b422ea7cdd85
jspark311 <josh.lindsay@gmail.com>
2016-02-15 22:25:18 -0700

    Fixing some FreeRTOS problems.

--- src/SpatiumGestu.cpp
+++ src/SpatiumGestu.cpp
@@ -1,142 +1,35 @@
+/*
+ * Example to demonstrate thread definition, semaphores, and thread sleep.
+ */
+#include <FreeRTOS_ARM.h>
 #include "FirmwareDefs.h"
 #include <ManuvrOS/Platform/Platform.h>
 #include <ManuvrOS/Kernel.h>
 #include <DataStructures/StringBuilder.h>
 
-#include <ManuvrOS/Drivers/MCP73833/MCP73833.h>
-#include <ManuvrOS/Drivers/MGC3130/MGC3130.h>
-#include <ManuvrOS/Drivers/ADP8866/ADP8866.h>
-#include <ManuvrOS/Drivers/INA219/INA219.h>
 
-#define HOST_BAUD_RATE  115200
+// The LED is attached to pin 13 on Arduino.
+const uint8_t PIN_LED1 = 13;
 
 
 Kernel* kernel = NULL;
 
-
-#if defined(__MK20DX256__) | defined(__MK20DX128__)
-const int PIN_LED1        = 13;
-const int MANUVR_LOGO_LED = 4;
-
-#include <FreeRTOS_ARM.h>
-
-void mainTaskFxn( void *pvParameters );
-void schedulerTaskFxn( void *pvParameters );
-void debugDumpTaskFxn( void *pvParameters );
-void serialIOTaskFxn( void *pvParameters );
-void loggerTask( void *pvParameters );
-
-
-#else
-const int MANUVR_LOGO_LED = 4;
-
-uint32_t timerCallbackScheduler(uint32_t currentTime) {  
-  if (kernel) kernel->advanceScheduler(); 
-  return (currentTime + (CORE_TICK_RATE * 1));
-}
-#endif
-
-
-uint8_t analog_write_val = 0;
-int8_t direction = 1;   // Zero for hold, 1 for brighten, -1 for darken.
-
-
-
 void blink_led() {  digitalWrite(PIN_LED1, !digitalRead(PIN_LED1));  }
 
-void logo_fade() {
-  if (direction < 0) analog_write_val--;
-  else if (direction > 0) analog_write_val++;
-  else return;
-  
-  if (0 == analog_write_val) {
-    direction = direction * -1;
-  }
-  else if (200 == analog_write_val) {
-    direction = direction * -1;
-  }
-  analogWrite(MANUVR_LOGO_LED, analog_write_val);
-}
 
 
-TaskHandle_t logger_pid = 0;
-TaskHandle_t kernel_pid = 0;
-
-
-void setup() {
-  Serial.begin(HOST_BAUD_RATE);   // USB
-
-  Kernel __kernel;  // Instance the kernel.
-  kernel = &__kernel;
-
-  pinMode(PIN_LED1,         OUTPUT);
-  pinMode(MANUVR_LOGO_LED,  OUTPUT);
-
-
-
-  #if defined(__MK20DX256__) | defined(__MK20DX128__)
-  
-  // Create the main thread.
-  xTaskCreate(mainTaskFxn, "Main", 2000, NULL, 2, &kernel_pid);
-
-  // Create the scheduler thread. Let's see if this flies....
-  xTaskCreate(schedulerTaskFxn, "Sched", 2000, (void*)kernel, 1, NULL );
-  xTaskCreate(debugDumpTaskFxn, "Debug", 2000, (void*)kernel, 1, NULL );
-  xTaskCreate(serialIOTaskFxn, "Serial", 512,  (void*)kernel, 1, NULL );
-  
-  xTaskCreate(loggerTask, "Logger", 512, (void*)kernel, 3, &logger_pid);
-  __kernel.provideKernelPID(kernel_pid);
-  __kernel.provideLoggerPID(logger_pid);
-
-
-  kernel->createSchedule(40, -1, false, logo_fade);
-  kernel->createSchedule(25, -1, false, blink_led);
-
-//  mcp73833 = new MCP73833(9, 10);
-  
-  // Setup the first i2c adapter and Subscribe it to Kernel.
-  I2CAdapter i2c(0);
-//  mgc3130 = new MGC3130(16, 17);
-
-  //INA219 ina219(0x4A);
-  ADP8866 adp8866(7, 8, 0x27);
-
-  __kernel.subscribe((EventReceiver*) &i2c);
-  __kernel.subscribe((EventReceiver*) &adp8866);
-
-  //i2c.addSlaveDevice(&ina219);
-  i2c.addSlaveDevice(&adp8866);
-  
-//  mgc3130->init();
-
-  __kernel.bootstrap();
-  vTaskStartScheduler();
- 
-  for(;;);
-  
-  #elif defined(_BOARD_FUBARINO_MINI_)
-  attachCoreTimerService(timerCallbackScheduler);
-  sei();
-  #endif
-}
-
-
-
-
-#if defined(__MK20DX256__) | defined(__MK20DX128__)
-
-void mainTaskFxn(void *pvParameters) {
-  int k_return = 0;
-  
-  /* As per most tasks, this task is implemented in an infinite loop. */
-  for(;;) {
-    taskENTER_CRITICAL();
-    k_return = kernel->procIdleFlags();
-    taskEXIT_CRITICAL();
-
-    if (0 == k_return) {
-      vTaskSuspend(NULL);   // Nothing more to do.
-    }
+// Declare a semaphore handle.
+SemaphoreHandle_t sem;
+//------------------------------------------------------------------------------
+/*
+ * Thread 1, turn the LED off when signalled by thread 2.
+ */
+// Declare the thread function for thread 1.
+static void Thread1(void* arg) {
+  while (1) {
+    // Wait for signal from thread 2.
+    xSemaphoreTake(sem, portMAX_DELAY);
+    vTaskDelay( 10 / portTICK_PERIOD_MS );
   }
 }
 
@@ -144,18 +37,21 @@ void mainTaskFxn(void *pvParameters) {
 void serialIOTaskFxn(void *pvParameters) {
   unsigned char* ser_buffer = (unsigned char*) alloca(255);
   int bytes_read = 0;
+  char tmp_char  = 0;
   for(;;) {
     if (Serial.available()) {
+      blink_led();
       // Zero the buffer.
-      bytes_read = 0;
-      for (int i = 0; i < 255; i++) *(ser_buffer+i) = 0;
-      char c = 0;
       while (Serial.available()) {
-        c = Serial.read();
-        *(ser_buffer+bytes_read++) = c;
+        tmp_char = Serial.read();
+        *(ser_buffer+bytes_read++) = tmp_char;
+        if (tmp_char == '\r' || tmp_char == '\n') {
+          *(ser_buffer+bytes_read) = 0;
+          kernel->accumulateConsoleInput(ser_buffer, bytes_read, true);
+          bytes_read = 0;
+          for (int i = 0; i < 255; i++) *(ser_buffer+i) = 0;
+        }
       }
-      
-      kernel->accumulateConsoleInput(ser_buffer, bytes_read, (c == '\r' || c == '\n'));
     }
     else {
       vTaskDelay(11 / portTICK_PERIOD_MS);
@@ -164,23 +60,12 @@ void serialIOTaskFxn(void *pvParameters) {
 }
 
 
-void schedulerTaskFxn(void *pvParameters) {
+static void schedulerTaskFxn(void *pvParameters) {
   for(;;) {
     // TODO: This sucks. There must be a better way of having the kernel's
     //   sense of time not being subservient to FreeRTOS's...
     kernel->advanceScheduler();
-    taskENTER_CRITICAL();
-    taskEXIT_CRITICAL();
     vTaskDelay( 10 / portTICK_PERIOD_MS );
-  }
-}
-
-void debugDumpTaskFxn(void *pvParameters) {
-  StringBuilder output;
-  for(;;) {
-    kernel->printDebug(&output);
-    Kernel::log(&output);
-    vTaskDelay( 2000 / portTICK_PERIOD_MS );
   }
 }
 
@@ -189,50 +74,81 @@ void loggerTask(void *pvParameters) {
   for(;;) {
     if (Kernel::log_buffer.count()) {
       if (!kernel->getVerbosity()) {
-        taskENTER_CRITICAL();
         Kernel::log_buffer.clear();
-        taskEXIT_CRITICAL();
       }
       else {
         Serial.print((char*) Kernel::log_buffer.position(0));
-        taskENTER_CRITICAL();
         Kernel::log_buffer.drop_position(0);
-        taskEXIT_CRITICAL();
       }
     }
     else {
-      vTaskSuspend(NULL);   // Nothing more to do.
+      //vTaskSuspend(NULL);   // Nothing more to do.
+      vTaskDelay( 10 / portTICK_PERIOD_MS );
     }
   }
 }
 
-void loop() {
-}
 
+//------------------------------------------------------------------------------
+/*
+ * Thread 2, turn the LED on and signal thread 1 to turn the LED off.
+ */
+// Declare the thread function for thread 2.
+static void Thread2(void* arg) {
 
-#elif defined(_BOARD_FUBARINO_MINI_)
+  pinMode(PIN_LED1, OUTPUT);
 
-void loop() {
-  unsigned char* ser_buffer = (unsigned char*) alloca(255);
-  int bytes_read = 0;
+  while (1) {
+    // Sleep for 200 milliseconds.
+    vTaskDelay((200L * configTICK_RATE_HZ) / 1000L);
+    kernel->procIdleFlags();
+    kernel->advanceScheduler();
+    Serial.println(F("Hold"));
 
-  while (1) {   // Service this loop for-ev-ar
-    while (Serial.available() == 0) {
-      kernel->procIdleFlags();
-      kernel->serviceScheduledEvents();
-    }
-
-    // Zero the buffer.
-    bytes_read = 0;
-    for (int i = 0; i < 255; i++) *(ser_buffer+i) = 0;
-    char c = 0;
-    while (Serial.available()) {
-      c = Serial.read();
-      *(ser_buffer+bytes_read++) = c;
-    }
-    
-    kernel->feedUSBBuffer(ser_buffer, bytes_read, (c == '\r' || c == '\n'));
+    // Signal thread 1 to turn LED off.
+    xSemaphoreGive(sem);
   }
 }
 
-#endif
+
+
+void setup() {
+  portBASE_TYPE s1, s2, s3, s4, s5;
+  
+  TaskHandle_t logger_pid = 0;
+  TaskHandle_t kernel_pid = 0;
+  
+  Serial.begin(115200);
+  kernel = new Kernel();
+  
+  // initialize semaphore
+  sem = xSemaphoreCreateCounting(1, 0);
+
+  // create task at priority two
+  s1 = xTaskCreate(Thread1, NULL, configMINIMAL_STACK_SIZE, NULL, 2, NULL);
+
+  // create task at priority one
+  s2 = xTaskCreate(Thread2, NULL, 3076, NULL, 1, &kernel_pid);
+  
+  s3 = xTaskCreate(loggerTask, NULL, 1024, NULL, 1, &logger_pid);
+  s4 = xTaskCreate(serialIOTaskFxn, NULL, 1024, NULL, 1, &logger_pid);
+  s5 = xTaskCreate(schedulerTaskFxn, NULL, 128, NULL, 1, NULL);
+
+  // check for creation errors
+  if (sem== NULL || s1 != pdPASS || s2 != pdPASS || s3 != pdPASS ) {
+    Serial.println(F("Creation problem"));
+    while(1);
+  }
+  
+  //kernel->createSchedule(100, -1, false, blink_led);
+
+  // start scheduler
+  kernel->bootstrap();
+  kernel->provideKernelPID(kernel_pid);
+  kernel->provideLoggerPID(logger_pid);
+
+  while(1);
+}
+
+void loop() {
+}
